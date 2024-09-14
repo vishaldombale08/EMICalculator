d8499cd43c4986947939694d93336d63c975801d
Vishal Dombale <79363597+vishaldombale08@users.noreply.github.com>
2024-09-15 00:58:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function calculateclick(){
     let P = parseInt(document.getElementById("txtAmount").value);
     let R = parseFloat(document.getElementById("txtRate").value)/12/100;
     let N = parseInt(document.getElementById("txtYears").value)*12;
-    let EMI = P * R * Math.pow(1+R, N)/ Math.pow(1+R,N)-1;
+    let EMI = P * R * [Math.pow(1+R, N)]/ [Math.pow(1+R,N)-1];
     document.getElementById("result").innerHTML = `Your Monthly Installment Amount is <b>&#8377; ${Math.round(EMI)}</b> for <b>${N}</b> Months`
 }
